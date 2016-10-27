71965df9db2f0e0bf9cef1ded35b7fddffd7dd7a
Rodrigo <rodsordi@hotmail.com>
2016-10-27 11:31:24 -0200

    First Commit

---
+++ src/Util/CalendarTests.java
@@ -0,0 +1,13 @@
+package Util;
+
+import java.util.Calendar;
+
+public class CalendarTests {
+	public static void main(String[] args) {
+		testAddDay();
+	}
+	
+	public static void testAddDay(){
+		Calendar calendar = Calendar.getInstance();
+	}
+}
